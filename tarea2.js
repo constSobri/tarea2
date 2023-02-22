1d1852de8b14acd1982de066229bd5e0dc6eac30
Sobri <57077187+constSobri@users.noreply.github.com>
2023-02-21 21:01:07 -0600

    Add files via upload

---
+++ tarea2.js
@@ -0,0 +1,12 @@
+let libroFavorito = {
+    "Titulo": "Rebelion de la granja",
+    "Autor": "George Orwell",
+    "Fecha de publicacion": new Date(1945,7,17),
+    "Url": "https://es.wikipedia.org/wiki/Rebeli%C3%B3n_en_la_granja"
+};
+
+console.log(libroFavorito["Fecha de publicacion"]);
+
+const datos = ["Angel", 32, true, new Date(2000,2,26), libroFavorito];
+
+console.log(datos)
